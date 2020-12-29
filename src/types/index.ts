a5f6f920edb3dd38fe7c212e98813b871d38353d
k-810 <k810marina@gmail.com>
2020-12-30 00:21:13 +0900

    types

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export type Member = {
+  name: string;
+  avatarSrc: string;
+  sources: string[];
+  githubUsername: string;
+  twitterUsername: string;
+
+};
+
+export type PostItem = {
+  authorName: string;
+  title: string;
+  link: string;
+  contentSnippet?: string;
+  isoDate?: string;
+  dateMiliSeconds: number;
+};
+
+export type FeedItem = {
+  title: string;
+  link: string;
+  contentSnippet?: string;
+  isoDate?: string;
+  dateMiliSeconds: number;
+};
+
+export type Category {
+  name: string;
+}
+
+
+export type Article {
+  id: string;
+  title: string;
+  body: string;
+  publishedAt: string;
+  category: Category | null;
+}
