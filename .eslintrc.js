6d8c723b90a05e5b7c01ac99b9b669d1e55188dc
k-810 <k810marina@gmail.com>
2021-01-25 00:45:32 +0900

    ignoreファイル修正, eslintrc修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,8 @@ module.exports = {
   extends: ["react-app", "plugin:jsx-a11y/recommended"],
   plugins: ["jsx-a11y"],
   rules: {
-    "import/no-anonymous-default-export": "error",
     "import/no-webpack-loader-syntax": "off",
-    "react/react-in-jsx-scope": "off", // React is always in scope with Blitz
-    "jsx-a11y/anchor-is-valid": "off", //Doesn't play well with Blitz/Next <Link> usage
+    "jsx-a11y/anchor-is-valid": "off", //Doesn't play well with Next <Link> usage
   },
   overrides: [
     {
