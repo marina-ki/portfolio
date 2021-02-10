a1efb7858108bd051bda3e3e34c19cf76a5ef668
k-810 <k810marina@gmail.com>
2021-02-11 05:03:20 +0900

    矢印のアイコン

---
+++ src/components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react"
+
+export const ChevronLeftIcon: FC = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+    </svg>
+  )
+}
---
+++ src/components/icons/ChevronRightIcon.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react"
+
+export const ChevronRightIcon: FC = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+    </svg>
+  )
+}
