ac13a076755e7bab400621f80bd1bec2b5299e23
k-810 <k810marina@gmail.com>
2021-02-11 05:33:52 +0900

    アイコンにclassName渡す

--- src/components/icons/ChevronLeftIcon.tsx
+++ src/components/icons/ChevronLeftIcon.tsx
@@ -1,8 +1,18 @@
-import React, { FC } from "react"
+import React, { FC, SVGAttributes } from "react"
 
-export const ChevronLeftIcon: FC = (props) => {
+type Props = {
+  className?: SVGAttributes<SVGSVGElement>["className"]
+}
+
+export const ChevronLeftIcon: FC<Props> = ({ className }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={className}
+    >
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
     </svg>
   )
