86d9818a70d0a3b3790c2857e559838d4987b68d
k-810 <k810marina@gmail.com>
2021-02-11 05:34:04 +0900

    Paginationのスタイル

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ["responsive", "hover", "focus", "group-hover"],
+      borderColor: ["responsive", "hover", "focus", "group-hover"],
+      borderWidth: ["responsive", "hover", "focus", "group-hover"],
+      margin: ["responsive", "hover", "focus", "group-hover"],
+      padding: ["responsive", "hover", "focus", "group-hover"],
+    },
   },
   plugins: [require("@tailwindcss/typography")],
-};
+}
