475cb9579ea1df5c62dc8c9569f2b693d964352c
k-810 <k810marina@gmail.com>
2021-05-08 03:12:40 +0900

    add frontend engineer

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,5 @@
+import { AboutItem, Header, Meta, QualificationList, SkillList, Title } from "components"
 import { getBlog } from "lib"
-import { Header, QualificationList, Title, AboutItem, SkillList, Meta } from "components"
 
 const contents = [
   { title: "東京大学理科一類 入学", date: "2017.04" },
@@ -22,6 +22,11 @@ const contents = [
       "デザインが形になるところに感動を感じ，フロントエンドエンジニア・モバイルエンジニアになろうと決める。主にReact Nativeを使用してアプリ開発をする。",
   },
   { title: "復学", date: "2020.10" },
+  {
+    title: "フロントエンドエンジニア",
+    date: "2021.01",
+    body: "Blitz.jsに惹かれ、Blitz.jsを使ってwebサービスを開発。",
+  },
 ]
 
 export default function About() {
@@ -33,7 +38,7 @@ export default function About() {
         image="https://images.microcms-assets.io/protected/ap-northeast-1:7b46820b-9e1b-4aab-ba38-e994b4176f3c/service/marina/media/marinya.png"
       />
       <Header />
-      <main className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 mb-10">
+      <main className="max-w-3xl px-4 mx-auto mb-10 sm:px-6 xl:max-w-5xl xl:px-0">
         <section className="border-b-2 border-gray-200">
           <Title>Skills</Title>
           <SkillList />
@@ -43,7 +48,7 @@ export default function About() {
           <QualificationList />
         </section>
         <Title>Education ＆ Career</Title>
-        <ul className="bg-gray-50 rounded-3xl p-2 sm:p-5 xl:p-6">
+        <ul className="p-2 bg-gray-50 rounded-3xl sm:p-5 xl:p-6">
           {contents.map((content, i) => (
             <AboutItem
               date={content.date}
