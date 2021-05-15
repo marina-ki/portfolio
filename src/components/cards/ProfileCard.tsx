0623f0ceca85b1cca8d4b39e9bd34d32a9c80729
k-810 <k810marina@gmail.com>
2021-05-15 12:50:05 +0900

    use next/image

--- src/components/cards/ProfileCard.tsx
+++ src/components/cards/ProfileCard.tsx
@@ -1,4 +1,5 @@
 import { EmailIcon, GithubIcon, ProfileIcon, TwitterIcon, ZennIcon } from "components"
+import Image from "next/image"
 import React, { FC } from "react"
 
 type Props = {}
@@ -7,15 +8,21 @@ export const ProfileCard: FC<Props> = (props) => {
   const {} = props
 
   return (
-    <div className="flex flex-col items-center justify-center bg-white p-4 shadow-lg rounded-lg border border-gray-200">
-      <div className="shadow-xl border border-gray-200 rounded-full overflow-hidden h-40 w-40">
-        <img src="/neko.jpg" alt="kamichan" className="h-40 w-40 object-contain" />
+    <div className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-lg">
+      <div className="w-40 h-40 overflow-hidden border border-gray-200 rounded-full shadow-xl">
+        <Image
+          src="/neko.jpg"
+          alt="neko icon"
+          className="object-contain w-40 h-40"
+          width="160"
+          height="160"
+        />
       </div>
 
-      <h2 className="mt-4 font-bold text-xl">Marina</h2>
+      <h2 className="mt-4 text-xl font-bold">Marina</h2>
       <p className="mt-2 text-sm font-medium">Mobile Engineer | Backend Engineer</p>
 
-      <p className="text-xs text-gray-500 text-center mt-3">React Native Lover</p>
+      <p className="mt-3 text-xs text-center text-gray-500">React Native Lover</p>
 
       <ul className="flex flex-row mt-4 space-x-2">
         <ProfileIcon href="https://github.com/marina-ki" name="github">
