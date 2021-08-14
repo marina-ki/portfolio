7e12cfe17257ccef3bb2a96c90d2f9b40747ced4
marina-ki <k810marina@gmail.com>
2021-08-14 16:44:03 +0900

    add read articles

---
+++ src/components/icons/ExternalLinkIcon.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from "react"
+
+type Props = {}
+
+export const ExternalLinkIcon: FC<Props> = (props) => {
+  const {} = props
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+      />
+    </svg>
+  )
+}
--- src/components/templates/BlogIndex.tsx
+++ src/components/templates/BlogIndex.tsx
@@ -1,4 +1,5 @@
-import { ArticleIndexCard, Header, Meta, Pagination } from "components"
+import { ArticleIndexCard, ExternalLinkIcon, Header, Meta, Pagination } from "components"
+import Link from "next/link"
 import React, { FC } from "react"
 import { Article } from "types"
 import { BLOG_PER_PAGE } from "utils"
@@ -18,8 +19,16 @@ export const BlogIndex: FC<Props> = ({ contents, totalCount, currentPage }) => {
         image="https://images.microcms-assets.io/protected/ap-northeast-1:7b46820b-9e1b-4aab-ba38-e994b4176f3c/service/marina/media/marinya.png"
       />
       <Header />
-      <main className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 mt-10">
-        <ul className="grid sm:grid-cols-2 xl:grid-cols-3 gap-6">
+      <main className="max-w-3xl px-4 mx-auto mt-10 sm:px-6 xl:max-w-5xl xl:px-0">
+        <section className="mb-10">
+          <Link href="https://tech.marinya.dev">
+            <a className="inline-flex text-blue-400">
+              <p>読んだ記事一覧</p>
+              <ExternalLinkIcon />
+            </a>
+          </Link>
+        </section>
+        <ul className="grid gap-6 sm:grid-cols-2 xl:grid-cols-3">
           {contents.map((article) => (
             <li key={article.id}>
               <ArticleIndexCard article={article} />
