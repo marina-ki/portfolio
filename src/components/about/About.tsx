447a1b91c90e35ecd739b53c09dc5afc89a8ba52
marina-ki <k810marina@gmail.com>
2023-11-30 17:13:37 +0900

    remove articles tab

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,26 @@
-import { career, qualifications, trainingPrograms } from "@src/lib"
-import { AboutItem, AboutSection, HackathonList, LinkList, SkillList } from "components"
+import posts from "@src/builder/posts"
+import { career, getBlog, qualifications, trainingPrograms } from "@src/lib"
+import { Article, PostItem } from "@src/types"
+import { BLOG_PER_PAGE } from "@src/utils"
+import {
+  AboutItem,
+  AboutSection,
+  BlogIndex,
+  HackathonList,
+  LinkList,
+  PostList,
+  SkillList,
+} from "components"
+import { GetStaticProps } from "next"
 
-export const About = () => {
+type Props = {
+  contents: any
+  totalCount: number
+}
+
+export const About = (props: Props) => {
+  const { contents, totalCount } = props
+
   return (
     <>
       <AboutSection title="Education">
@@ -40,6 +59,27 @@ export const About = () => {
       <AboutSection title="Skills">
         <SkillList />
       </AboutSection>
+
+      <AboutSection title="Articles">
+        <PostList items={posts as PostItem[]} />
+      </AboutSection>
+
+      <AboutSection title="Blog">
+        <BlogIndex {...{ contents, totalCount, currentPage: 1 }} />
+      </AboutSection>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const data: {
+    contents: Article[]
+    totalCount: number
+  } = await getBlog({ offset: 0, limit: BLOG_PER_PAGE })
+  return {
+    props: {
+      contents: data.contents,
+      totalCount: data.totalCount,
+    },
+  }
 }
